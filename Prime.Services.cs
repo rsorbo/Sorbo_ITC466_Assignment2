356fce91023005544e91706104f8067c492dfeac
rsorbo <robsorbo@gmail.com>
2021-09-26 19:31:56 -0500

    Add project files.

---
+++ Prime.Services.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Prime_Service
+{
+    class Prime_Service
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first.");
+        }
+
+
+    }
+
+    public class DivideService
+    {
+        public bool IsDivbyTwo(int candidate)
+        {
+            if ((candidate % 2) == 0)
+            {
+                return true;
+            }
+            throw new NotImplementedException("Please create a test first.");
+
+        }
+
+
+    }
+}
